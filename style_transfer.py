514f5925da2e1372b06c46c5fbdc5cc3d362db16
Davis Foote <djfoote@berkeley.edu>
2016-10-20 18:45:12 -0700

    audio style transfer main function outline. Does nothing.

---
+++ style_transfer.py
@@ -0,0 +1,24 @@
+import wavenet.model as wavenet
+
+import tensorflow as tf
+import numpy as np
+
+
+def style_transfer(
+        network,
+        content,
+        style,
+        iterations,
+        content_weight,
+        style_weight,
+        learning_rate):
+    """
+    """
+    # content features for content signal
+    # style features for style signal
+    # define signal variable
+    # content loss
+    # style loss
+    # overall loss
+    # optimizer setup
+    # optimization
